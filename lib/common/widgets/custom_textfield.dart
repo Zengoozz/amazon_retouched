457b3c4cff6a8f76075a65ddcb7c0b8890a58dc2
Zengoozz <classicalegyptian@gmail.com>
2022-07-27 09:49:41 +0200

    adding custom txtField

--- lib/common/widgets/custom_textfield.dart
+++ lib/common/widgets/custom_textfield.dart
@@ -24,7 +24,12 @@ class CustomTextField extends StatelessWidget {
             ),
           ),
         ),
-        // validator: ,
+        validator:(val){
+          if(val == null || val.isEmpty){
+            return 'Enter your $hintText';
+          }
+          return null;
+        },
       ),
     );
   }
